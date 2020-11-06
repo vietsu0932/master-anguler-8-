873ea28cc12eeda1c243572c95af5d74d782cf5f
dvsu <sudinhviet20@gmail.com>
2020-11-06 14:29:05 +0700

    Creating component by using Angular CLI

--- based-project/src/app/app.module.ts
+++ based-project/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartHeaderComponent } from './cart-header/cart-header.component';
+import { CartSummaryComponent } from './cart-summary/cart-summary.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { PromoteCodeComponent } from './promote-code/promote-code.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    CartHeaderComponent,
+    CartSummaryComponent,
+    ProductListComponent,
+    PromoteCodeComponent
   ],
   imports: [
     BrowserModule
---
+++ based-project/src/app/promote-code/promote-code.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promote-code',
+  templateUrl: './promote-code.component.html',
+})
+export class PromoteCodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
